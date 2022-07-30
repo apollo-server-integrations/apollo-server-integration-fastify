2a5ec02da1fffcb3996e574011c41c192e12ce1b
Oliver Plummer <oliver.plummer@outlook.com>
2022-07-31 04:46:42 +1000

    Update

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,14 +1,8 @@
-// import fp, { PluginMetadata } from "fastify-plugin"
-import type { FastifyPluginAsync, RawServerBase, RawServerDefault } from "fastify"
 import type { ApolloServer, BaseContext } from "@apollo/server"
+import type { FastifyPluginAsync, RawServerBase, RawServerDefault } from "fastify"
 
 import { fastifyApolloHandler } from "./handler"
 import { ApolloFastifyPluginOptions } from "./types"
-
-// const pluginMetadata: PluginMetadata = {
-// 	fastify: "4.x",
-// 	name: "apollo-server-fastify",
-// }
 
 export const fastifyApollo = <Context extends BaseContext = BaseContext, RawServer extends RawServerBase = RawServerDefault>(
 	apollo: ApolloServer<Context>,
