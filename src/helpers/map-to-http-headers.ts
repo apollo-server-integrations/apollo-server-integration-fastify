c3e33b0ca4e677efb14546881685641d3e4f00f1
Oliver Plummer <oliver.plummer@outlook.com>
2022-07-31 03:02:58 +1000

    Update

--- src/helpers/map-to-http-headers.ts
+++ src/helpers/map-to-http-headers.ts
@@ -1,9 +1,8 @@
-import type { IncomingHttpHeaders } from "node:http"
 import type { HTTPGraphQLRequest } from "@apollo/server"
 
 export const mapToHttpHeaders =
 	(map: HTTPGraphQLRequest["headers"]) => {
-		const headers: IncomingHttpHeaders = {}
+		const headers: Record<string, unknown> = {}
 
 		for (const [key, value] of map) {
 			headers[key] = value
