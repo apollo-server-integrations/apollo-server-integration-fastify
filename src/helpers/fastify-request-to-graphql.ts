f8598d924dc613a49487819dae48e7a4534c9ce2
Oliver Plummer <oliver.plummer@outlook.com>
2022-08-02 20:45:21 +1000

    Update

--- src/helpers/fastify-request-to-graphql.ts
+++ src/helpers/fastify-request-to-graphql.ts
@@ -5,9 +5,17 @@ import type { FastifyRequest, RawServerBase, RawServerDefault } from "fastify"
 import { httpHeadersToMap } from "./http-headers-to-map"
 
 export const fastifyRequestToGraphQL =
-	<RawServer extends RawServerBase = RawServerDefault>(request: FastifyRequest<RouteGenericInterface, RawServer>): HTTPGraphQLRequest => ({
-		body: request.body,
-		method: request.method.toUpperCase(),
-		search: request.params as string ?? "",
-		headers: httpHeadersToMap(request.headers),
-	})
+	<RawServer extends RawServerBase = RawServerDefault>(request: FastifyRequest<RouteGenericInterface, RawServer>): HTTPGraphQLRequest => {
+		console.log({
+			"request.url": request.url,
+			"request.query": request.query,
+			"request.params": request.params,
+			"new URL(request.url).search": new URL(request.url).search,
+		})
+		return ({
+			body: request.body,
+			method: request.method.toUpperCase(),
+			search: request.query as string ?? "",
+			headers: httpHeadersToMap(request.headers),
+		})
+	}
