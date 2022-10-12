ab2ffcb3564b6b045cd2c0d6baa2e553e7888afa
Oliver Plummer <oliver.plummer@outlook.com>
2022-10-13 09:45:35 +1100

    Upgrade for Apollo Server v4

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./types";
+// eslint-disable-next-line no-restricted-exports
 export { fastifyApollo as default } from "./plugin";
 export * from "./handler";
 export * from "./drain-plugin";
